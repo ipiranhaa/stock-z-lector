9e85c1115f4e6e81259d7d891812738aa3f5880d
ipiranhaa <tanatezp@gmail.com>
2021-02-03 22:41:55 +0700

    Init browser

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log("Hello world");
+const puppeteer = require('puppeteer')
+
+;(async () => {
+  const browser = await puppeteer.launch()
+  console.log(await browser.version())
+  const page = await browser.newPage()
+  await page.goto(
+    'https://marketdata.set.or.th/mkt/sectorquotation.do?sector=SET50&language=th&country=TH'
+  )
+  const element = await page.$x('//th[text()="หลักทรัพย์"]')
+  console.log(element)
+  // other actions...
+  await browser.close()
+})()
