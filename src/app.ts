d2fc10ba24cdbde58a005c6970bb682dd1f7dcaa
ipiranhaa <tanatezp@gmail.com>
2021-07-03 10:02:02 +0700

    fix: job timeout

--- src/app.ts
+++ src/app.ts
@@ -13,65 +13,65 @@ import {
 import { getStockEvent } from './settrade'
 import { sendSlack } from './slack'
 ;(async () => {
-  const browser: Browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox', '--start-maximized'],
-  })
+  try {
+    const browser: Browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--start-maximized'],
+    })
 
-  const [set50Stocks, set100Stocks, setHDStocks, maiStocks] = await Promise.all([
-    getStockByIndex(browser, 'SET50'),
-    getStockByIndex(browser, 'SET100'),
-    getStockByIndex(browser, 'SETHD'),
-    getMAIStock(browser),
-  ])
-  const summaryStocks = [...set100Stocks, ...maiStocks]
+    const [set50Stocks, set100Stocks, setHDStocks, maiStocks] = await Promise.all([
+      getStockByIndex(browser, 'SET50'),
+      getStockByIndex(browser, 'SET100'),
+      getStockByIndex(browser, 'SETHD'),
+      getMAIStock(browser),
+    ])
+    const summaryStocks = [...set100Stocks, ...maiStocks]
 
-  const [
-    allStockProfiles,
-    allJittaStockDetail,
-    allStockDividendDetail,
-    set100TechnicalStocks,
-  ] = await Promise.all([
-    getStockProfile(browser, summaryStocks),
-    getAllStockDetail(browser, summaryStocks),
-    getStockEvent(browser, summaryStocks),
-    getStockTechnical(browser, set100Stocks),
-  ])
+    const [allStockProfiles, allJittaStockDetail, allStockDividendDetail, set100TechnicalStocks] =
+      await Promise.all([
+        getStockProfile(browser, summaryStocks),
+        getAllStockDetail(browser, summaryStocks),
+        getStockEvent(browser, summaryStocks),
+        getStockTechnical(browser, set100Stocks),
+      ])
 
-  await browser.close()
+    await browser.close()
 
-  // Note: Manual resolve advice because tradingview does not have some mai stocks detail.
-  const maiTechnicalStocks = maiStocks.reduce((summary, name) => {
-    const manipulatedSummary = summary
-    manipulatedSummary[name] = {
-      advice: '',
-    }
-    return manipulatedSummary
-  }, {} as TradingViewStock)
-  const allTechnicalStocks: TradingViewStock = { ...set100TechnicalStocks, ...maiTechnicalStocks }
+    // Note: Manual resolve advice because tradingview does not have some mai stocks detail.
+    const maiTechnicalStocks = maiStocks.reduce((summary, name) => {
+      const manipulatedSummary = summary
+      manipulatedSummary[name] = {
+        advice: '',
+      }
+      return manipulatedSummary
+    }, {} as TradingViewStock)
+    const allTechnicalStocks: TradingViewStock = { ...set100TechnicalStocks, ...maiTechnicalStocks }
 
-  const mergedStockDetail: StockDetail[] = allJittaStockDetail.map((jittaDetail) => ({
-    ...jittaDetail,
-    ...allTechnicalStocks[jittaDetail.name],
-    ...allStockProfiles[jittaDetail.name],
-    ...allStockDividendDetail[jittaDetail.name],
-  }))
+    const mergedStockDetail: StockDetail[] = allJittaStockDetail.map((jittaDetail) => ({
+      ...jittaDetail,
+      ...allTechnicalStocks[jittaDetail.name],
+      ...allStockProfiles[jittaDetail.name],
+      ...allStockDividendDetail[jittaDetail.name],
+    }))
 
-  const sortedAllResult = prioratiseStock(mergedStockDetail)
-  const [sortedSET100Result, sortedMAIResult] = parsingSETAndMAIStocks(sortedAllResult, maiStocks)
-  const sortedSET50Result = sortedSET100Result.filter((stock: JittaStockDetail) =>
-    set50Stocks.includes(stock.name.toUpperCase())
-  )
-  const sortedSETHDResult = sortedSET100Result.filter((stock: JittaStockDetail) =>
-    setHDStocks.includes(stock.name.toUpperCase())
-  )
+    const sortedAllResult = prioratiseStock(mergedStockDetail)
+    const [sortedSET100Result, sortedMAIResult] = parsingSETAndMAIStocks(sortedAllResult, maiStocks)
+    const sortedSET50Result = sortedSET100Result.filter((stock: JittaStockDetail) =>
+      set50Stocks.includes(stock.name.toUpperCase())
+    )
+    const sortedSETHDResult = sortedSET100Result.filter((stock: JittaStockDetail) =>
+      setHDStocks.includes(stock.name.toUpperCase())
+    )
 
-  const formattedSET100Json = stampDatetime(sortedSET100Result)
-  const formattedSET50Json = stampDatetime(sortedSET50Result)
-  const formattedSETHDJson = stampDatetime(sortedSETHDResult)
-  const formattedMAIJson = stampDatetime(sortedMAIResult)
+    const formattedSET100Json = stampDatetime(sortedSET100Result)
+    const formattedSET50Json = stampDatetime(sortedSET50Result)
+    const formattedSETHDJson = stampDatetime(sortedSETHDResult)
+    const formattedMAIJson = stampDatetime(sortedMAIResult)
 
-  writingManager(formattedSET100Json, formattedSET50Json, formattedSETHDJson, formattedMAIJson)
+    writingManager(formattedSET100Json, formattedSET50Json, formattedSETHDJson, formattedMAIJson)
 
-  sendSlack(sortedAllResult)
+    sendSlack(sortedAllResult)
+  } catch (error) {
+    process.exit(1)
+  }
 })()
